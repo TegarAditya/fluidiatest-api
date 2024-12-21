aa226ccd1a1201705149d305614eedd5aa53fe5c
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-21 12:07:58 +0700

    feat: add API endpoint to retrieve a single test by ID

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono"
-import { getTests } from "../handlers/test"
+import { getTest, getTests } from "../handlers/test"
 
 const api = new Hono()
 
 api.get("/tests", ...getTests)
+api.get("/test/:id", ...getTest)
 
 export default api
