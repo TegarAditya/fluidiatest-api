00327ee21762838351826e6735b410a5c0d05b83
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-19 22:34:18 +0700

    feat: add endpoint for creating test attempts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono"
-import { getTest, getTests } from "../handlers/test"
+import { createAttempt, getTest, getTests } from "../handlers/test"
 import { getUser, getUsers } from "../handlers/user"
 
 const api = new Hono()
@@ -9,5 +9,7 @@ api.get("/user/:id", ...getUser)
 
 api.get("/tests", ...getTests)
 api.get("/test/:id", ...getTest)
+
+api.post("/test/attempt", ...createAttempt)
 
 export default api
