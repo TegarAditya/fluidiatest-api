3c2324d34f2319a22a627c6484f5d2392796ca5f
Tegar Aditya <tegaraditya53@gmail.com>
2025-02-16 06:37:01 +0700

    feat: add endpoints for retrieving question lists and results

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,16 +1,22 @@
 import { Hono } from "hono"
-import { createAttempt, getTest, getTestResults, getTests } from "../handlers/test"
+import { createAttempt, getTestQuestionList, getTest, getTests } from "../handlers/test"
 import { getUser, getUsers } from "../handlers/user"
+import { getBanks } from "../handlers/bank"
+import { getResult, getResults } from "../handlers/result"
 
 const api = new Hono()
 
 api.get("/users", ...getUsers)
 api.get("/user/:id", ...getUser)
 
+api.get("/bank", ...getBanks)
+
 api.get("/tests", ...getTests)
 api.get("/test/:id", ...getTest)
-api.get("/tests/results", ...getTestResults)
-
+api.get("/test/:id/questions", ...getTestQuestionList)
 api.post("/test/attempt", ...createAttempt)
+
+api.get("/result", ...getResult)
+api.get("/results", ...getResults)
 
 export default api
