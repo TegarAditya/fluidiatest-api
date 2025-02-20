b63d6380dfbe964d5eecf2876e6fd965e8465950
Tegar Aditya <tegaraditya53@gmail.com>
2025-02-20 13:53:34 +0700

    feat: add optional exam_id query parameter to getBanks handler

--- src/handlers/bank.ts
+++ src/handlers/bank.ts
@@ -1,31 +1,47 @@
 import { createFactory } from "hono/factory"
 import prisma from "../libs/prisma"
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod"
 
 const factory = createFactory()
 
 //GET /api/bank
-export const getBanks = factory.createHandlers(async (c) => {
-  try {
-    const bank = await prisma.question_banks.findMany({
-      select: {
-        id: true,
-        code: true,
-      },
+export const getBanks = factory.createHandlers(
+  zValidator(
+    "query",
+    z.object({
+      exam_id: z.string().optional(),
     })
+  ),
+  async (c) => {
+    try {
+      const examId = c.req.query("exam_id")
 
-    if (bank.length <= 0) {
-      return c.json({ message: "No bank found" }, 404)
-    }
-
-    const result = bank.map((b) => {
-      return {
-        id: Number(b.id),
-        code: b.code,
+      if (examId) {
+        
       }
-    })
 
-    return c.json(result)
-  } catch (error: any) {
-    return c.json({ message: error.message }, 500)
+      const bank = await prisma.question_banks.findMany({
+        select: {
+          id: true,
+          code: true,
+        },
+      })
+
+      if (bank.length <= 0) {
+        return c.json({ message: "No bank found" }, 404)
+      }
+
+      const result = bank.map((b) => {
+        return {
+          id: Number(b.id),
+          code: b.code,
+        }
+      })
+
+      return c.json(result)
+    } catch (error: any) {
+      return c.json({ message: error.message }, 500)
+    }
   }
-})
+)
